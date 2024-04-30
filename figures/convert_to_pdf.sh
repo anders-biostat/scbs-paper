c675567b9b3c290bd8923689358bacbbbd1fb9a1
Lukas <l.kremer@dkfz.de>
2024-04-30 21:50:37 +0200

    maybe we dont need 3 references for seurat :D

--- figures/convert_to_pdf.sh
+++ figures/convert_to_pdf.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# sudo apt install librsvg2-bin
-#for file in *svg; do rsvg-convert -f pdf -o ${file%.*}.pdf $file; done
-
 for file in *svg; do inkscape $file --export-pdf=${file%.*}.pdf; done
 
 
