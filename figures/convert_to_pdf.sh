972f042dfd3c876685bd9c0e571a4eb3127cd43b
Lukas <l.kremer@dkfz.de>
2024-04-30 18:49:46 +0200

    fix pdf

--- figures/convert_to_pdf.sh
+++ figures/convert_to_pdf.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
 # sudo apt install librsvg2-bin
-for file in *svg; do rsvg-convert -f pdf -o ${file%.*}.pdf $file; done
+#for file in *svg; do rsvg-convert -f pdf -o ${file%.*}.pdf $file; done
+
+for file in *svg; do inkscape $file --export-pdf=${file%.*}.pdf; done
+
+
